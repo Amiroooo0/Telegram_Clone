2820ed70eba114e965c2bbb6c70b104a29b045cd
Amiroooo0 <mesterdidi13831383@gmail.com>
2025-06-09 18:52:05 +0330

    build.gradle and mudule-info.java fixed

--- Telegram/src/main/java/module-info.java
+++ Telegram/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module develop.telegram {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens develop.telegram to javafx.fxml;
-    exports develop.telegram;
+    opens develop.telegram.app to javafx.fxml;
+    exports develop.telegram.app;
+    opens develop.telegram.controller to javafx.fxml;
 }
